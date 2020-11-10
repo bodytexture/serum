0c8bc32f23a708f311d3a9086ccd98702589dfcf
carlo <carlo.travaglio@gmail.com>
2020-11-10 18:52:43 +0100

    Updates

---
+++ precache-manifest.61077344fed6df06e2083a456818c01c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd62a0433ce45d10793e31c9ab6c0da",
+    "url": "/superserum/index.html"
+  },
+  {
+    "revision": "58e1677216010c460e6e",
+    "url": "/superserum/static/css/2.bede2b44.chunk.css"
+  },
+  {
+    "revision": "d5760dc922ac481d8fda",
+    "url": "/superserum/static/css/main.370dcbf2.chunk.css"
+  },
+  {
+    "revision": "58e1677216010c460e6e",
+    "url": "/superserum/static/js/2.22888748.chunk.js"
+  },
+  {
+    "revision": "ed364e94a6f6b1bb953368935d752ffd",
+    "url": "/superserum/static/js/2.22888748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5760dc922ac481d8fda",
+    "url": "/superserum/static/js/main.8fce4d40.chunk.js"
+  },
+  {
+    "revision": "74e5783cfbee9891406c",
+    "url": "/superserum/static/js/runtime-main.e7a09621.js"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "/superserum/static/media/logo.49174c73.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bodytexture/serum/precache-manifest.3bf9a5e09d32c881f21f228214f8f32c.js"
+  "/superserum/precache-manifest.61077344fed6df06e2083a456818c01c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bodytexture/serum/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/superserum/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
