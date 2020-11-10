415f29f0b88822b1174e525c2a53994fbe818d40
carlo <carlo.travaglio@gmail.com>
2020-11-10 01:43:54 +0100

    Updates

---
+++ precache-manifest.3bf9a5e09d32c881f21f228214f8f32c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a247ce515afc13e1e306c2ac5a6d19",
+    "url": "/bodytexture/serum/index.html"
+  },
+  {
+    "revision": "b6474875d75257fb0794",
+    "url": "/bodytexture/serum/static/css/2.bede2b44.chunk.css"
+  },
+  {
+    "revision": "0f74712bdfecf26e75e1",
+    "url": "/bodytexture/serum/static/css/main.370dcbf2.chunk.css"
+  },
+  {
+    "revision": "b6474875d75257fb0794",
+    "url": "/bodytexture/serum/static/js/2.19a13d50.chunk.js"
+  },
+  {
+    "revision": "ed364e94a6f6b1bb953368935d752ffd",
+    "url": "/bodytexture/serum/static/js/2.19a13d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f74712bdfecf26e75e1",
+    "url": "/bodytexture/serum/static/js/main.f85fbbb5.chunk.js"
+  },
+  {
+    "revision": "736bc95e0342c505d5e9",
+    "url": "/bodytexture/serum/static/js/runtime-main.1cc786fa.js"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "/bodytexture/serum/static/media/logo.49174c73.svg"
+  }
+]);
